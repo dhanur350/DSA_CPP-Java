34ff5f27300bb7985d0b2ffa7fe40c89f3d782b4
dhanur350 <dhanur297@gmail.com>
2022-04-20 01:11:54 +0530

    string

---
+++ DSA/large.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class large {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n, i, largest = 0;
+        n = scan.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter numbers");
+        for (i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        for (i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            }
+        }
+        System.out.print(" " + largest);
+    }
+}
