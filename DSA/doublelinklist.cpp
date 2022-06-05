dbc3ba777e67330d0288297cc2d920dd09715e78
dhanur350 <dhanur297@gmail.com>
2022-06-05 23:28:04 +0530

    Day22.1

--- DSA/doublelinklist.cpp
+++ DSA/doublelinklist.cpp
@@ -7,10 +7,35 @@ struct node{
 }*first,*second,*third;
 //Either declare node like this
 int main(){
-    /* Or Declare Node like this
+    /* 
+    Or Declare Node like this
     struct node *one = NULL;
     struct node *two = NULL;
     struct node *three = NULL;
     */
+    first=NULL; //Initializing node
+    second = NULL;
+    third = NULL;
+    first=(struct node *)malloc(sizeof(struct node)); //Allocating memory
+    second =(struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    first->data=100;
+    first->prev=NULL;
+    first->next=second;
+    second->data=200;
+    second->prev=first;
+    second->next=third;
+    third->data=300;
+    third->prev=second;
+    third->next=NULL;
+    cout<<"First data "<<first->data<<endl;
+    cout<<"First prev node "<<first->prev<<endl;
+    cout<<"First link part "<<first->next<<endl;
+    cout<<"Second data "<<second->data<<endl;
+    cout<<"Second prev node "<<second->prev<<endl;
+    cout<<"Second link part "<<second->next<<endl;
+    cout<<"Third Data "<<third->data<<endl;
+    cout<<"Third prev node "<<third->prev<<endl;
+    cout<<"Third link part "<<third->next<<endl;
     return 0;
 }
---
+++ Simple Codes/arr.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    int arr[]={1,2,3,4,5};
+    for (size_t i = 0; i < 5; i++)
+    {
+        printf("%d",arr[i]);
+    }
+    
+}
