2d87ca8d239128219ccdb908c8200984f12f3b26
dhanur350 <dhanur297@gmail.com>
2022-07-16 22:34:19 +0530

    Day30

--- DSA/comparestr.java
+++ DSA/comparestr.java
@@ -1,3 +1,4 @@
+package DSA;
 import java.util.Scanner;
 
 public class comparestr {
--- Pointers/point.java
+++ Pointers/point.java
@@ -1,3 +1,4 @@
+package Pointers;
 public class point {
     public static void main(String[] args) {
         System.out.println("As Java has no pointer data types,\n it is impossible to use pointers in Java.");
