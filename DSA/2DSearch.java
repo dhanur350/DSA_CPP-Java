b1e2014835d2fef57a6b59a9f29b4a703c1f85a4
dhanur350 <dhanur297@gmail.com>
2023-04-16 00:00:37 +0530

    Day32

--- DSA/2DSearch.java
+++ DSA/2DSearch.java
@@ -5,15 +5,28 @@ import java.util.Scanner;
 class search{
     public static void main(String[] args) {
         int row,column;
+        Scanner scan = new Scanner(System.in);
         try {
-            Scanner scan = new Scanner(System.in);
             System.out.println("Enter the input?");
             row = scan.nextInt();
             column = scan.nextInt();
-            System.out.println(row+column);            
+            // System.out.println(row+column); 
+            int[][] matrix = new int[row][column];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < column; j++) {
+                    matrix[row][column] = scan.nextInt();
+                }
+            }
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < column; j++) {
+                    System.out.println(matrix[row][column]);
+                }
+            }
         } catch (Exception e) {
             // TODO: handle exception
+            System.err.println(e);
         }
+        scan.close();
         // System.out.println("hello");
     }
 }
