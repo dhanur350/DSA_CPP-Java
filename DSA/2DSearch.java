7e06b42f00e9a81fdf28c8f985cc8cec96464852
dhanur350 <dhanur297@gmail.com>
2023-04-30 15:28:09 +0530

    Day34

--- DSA/2DSearch.java
+++ DSA/2DSearch.java
@@ -1,4 +1,4 @@
-package DSA;
+// package DSA;
 
 import java.util.Scanner;
 
@@ -6,26 +6,36 @@ class search{
     public static void main(String[] args) {
         int row,column;
         Scanner scan = new Scanner(System.in);
-        try {
-            System.out.println("Enter the input?");
-            row = scan.nextInt();
-            column = scan.nextInt();
+        // try {
+            // System.out.println("Enter the input?");
+            // row = scan.nextInt();
+            row = 2; column = 2;
+            // column = scan.nextInt();
             // System.out.println(row+column); 
-            int matrix[][] = new int[row][column];
+            int[][] matrix = new int[row][column];
+
+            // System.out.println(matrix[1][1]);
             for (int i = 0; i < row; i++) {
                 for (int j = 0; j < column; j++) {
-                    matrix[row][column] = scan.nextInt();
+                    matrix[i][j] = scan.nextInt();
                 }
             }
-            for (int i = 0; i < row; i++) {
-                for (int j = 0; j < column; j++) {
-                    System.out.println(matrix[row][column]);
+            for (int i = 0; i < row - 1; i++) {
+                for (int j = 0; j < column - 1; j++) {
+                    System.out.println(matrix[i][j]);
                 }
             }
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.err.println(e);
-        }
+            int count = 0;
+            for (int i = 0; i < row - 1; i++) {
+                for (int j = 0; j < column - 1; j++) {
+                    if(matrix[i][j] == 1){
+                        count = count + 1;
+                    }
+                    System.out.print("\n");
+                }
+            }
+            System.out.println("no. of 1's" + count);
+        // }
         scan.close();
         // System.out.println("hello");
     }
