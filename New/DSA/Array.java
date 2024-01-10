330b34d501011c7e42a2305058037cd49ea775b8
Dhanur <71970682+dhanur350@users.noreply.github.com>
2024-01-10 12:32:15 +0000

    arrayjava

--- New/DSA/Array.java
+++ New/DSA/Array.java
@@ -1,6 +1,9 @@
+import java.util.*;
+
 public class learnArray {
     public static void main(String[] args) {
         System.out.println("");
-        int arr[] = new int[num]
+        int arr[] = new int[999];
+        
     }
 }
