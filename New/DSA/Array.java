facc1e1029c42e3b722b343173337bbe2c371666
Dhanur <71970682+dhanur350@users.noreply.github.com>
2024-08-04 15:25:31 +0000

    cpp commit

--- New/DSA/Array.java
+++ New/DSA/Array.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class learnArray {
     public static void main(String[] args) {
+        int arr[] = new int[];
 
         System.out.println("Array Practice in Java");
         int arr[] = new int[999];
